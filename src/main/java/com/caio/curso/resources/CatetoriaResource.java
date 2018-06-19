12534f814a1bf6b69ae88acb731cb904859fae82
Caio Malheiros <md.caio@yahoo.com.br>
2018-06-19 18:26:10 -0300

    Testando a primeira classe Categoria

--- src/main/java/com/caio/curso/resources/CatetoriaResource.java
+++ src/main/java/com/caio/curso/resources/CatetoriaResource.java
@@ -1,15 +1,37 @@
 package com.caio.curso.resources;
 
+import java.util.ArrayList;
+
+import java.util.List;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.caio.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CatetoriaResource {
 
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "Rest está funcionando";
+	public List<Categoria> listar() {
+	
+		Categoria cat1 = new Categoria();
+		Categoria cat2 = new Categoria();
+		
+		cat1.setId(1);
+		cat1.setNome("Mouse");
+		
+		cat2.setId(2);
+		cat2.setNome("Teclado");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+	
+		
+		return lista;
+		
+		
 	}
 }
